31081eefd33f0aa61002840f0dc42b6c1e59c617
Tyler Matthews <100244201+T-Matthews@users.noreply.github.com>
2022-09-23 17:20:13 -0700

    Nav Bar Component Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import React, { useState } from 'react';
+import Navbar from './components/navbar';
+
+
 
 function App() {
+const [count, setCount] =useState(0);
+const [students,setStudents] = useState(['Sven','Donovan','Tyler','Yasir','BT'])
+
+const changeCounter = () =>{
+  setCount(count +1)
+}
+
+function shuffleStudents() {
+  let tempStudents = [...students]
+  tempStudents.sort(() => Math.random()- 0.5)
+  setStudents(tempStudents)
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <h1 className='counter'>{count}</h1>
+      <button onClick={changeCounter}>Change Count!</button>
+      <h1 className='students'>{students}</h1>
+      <button onClick={shuffleStudents}>Shuffle Students!</button>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+let Navbar = () =>{
+
+    return (
+        <>
+        <div className="navbar">
+            <div className="navbar-left">
+                <a className="nav-item nav-item-l" id="nav-name" href="#">Tyler Matthews</a>
+            </div>
+            <div className="navbar-right">
+                <a className="nav-item nav-item-r" id="nav-projects" href="#">Projects</a>
+                <a className="nav-item nav-item-r" id="nav-previous" href="#">Previous Work</a>
+                <a className="nav-item nav-item-r" id="nav-hobbies" href="#">Hobbies</a>
+                <a className="nav-item nav-item-r" id="nav-contact" href="#">Contact</a>
+            </div>
+        </div>
+        <div className="underline">
+            <div className="underline-l"></div>
+            <div className="underline-r"></div>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
